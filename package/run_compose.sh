f59ddef3d560ee1ddc5748ee47a1913b39232061
yuetsin <akaza_akari@sjtu.edu.cn>
2019-07-11 14:19:19 +0800

    [fix] remove rubbish web front end

--- package/run_compose.sh
+++ package/run_compose.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-echo "[ ] removing web repo ... "
-rm -rf eyulingo-web
-echo "[x] removed. "
+# echo "[ ] removing web repo ... "
+# rm -rf eyulingo-web
+# echo "[x] removed. "
 
 echo "[ ] stopping docker-compose images ... "
 docker-compose stop
@@ -13,28 +13,28 @@ echo y | docker-compose rm
 echo "[*] removed docker-compose images"
 
 echo "[ ] removing obsolete images ... "
-docker images | grep -E "(eyulingo-server|eyulingo-mysql)" | awk '{print $3}' | uniq | xargs -I {} docker rmi --force {}
+docker images | grep -E "(eyulingo-server|eyulingo-mysql|eyulingo-admin|eyulingo-dist)" | awk '{print $3}' | uniq | xargs -I {} docker rmi --force {}
 echo "[*] removed obsolete images"
 
-echo "[ ] cloning front-end repo ... "
-git clone https://github.com/eyulingo/eyulingo-web
-echo "[x] cloned front-end repo"
+# echo "[ ] cloning front-end repo ... "
+# git clone https://github.com/eyulingo/eyulingo-web
+# echo "[x] cloned front-end repo"
 
-echo "[ ] building admin page ... "
+# echo "[ ] building admin page ... "
 
-cd ./eyulingo-web/eyulingo-admin/
-./build_docker.sh
+# cd ./eyulingo-web/eyulingo-admin/
+# ./build_docker.sh
 
-cd ../../
-echo "[x] successfully built admin page "
+# cd ../../
+# echo "[x] successfully built admin page "
 
-echo "[ ] building distributor page ... "
+# echo "[ ] building distributor page ... "
 
-cd ./eyulingo-web/eyulingo-dist/
-./build_docker.sh
+# cd ./eyulingo-web/eyulingo-dist/
+# ./build_docker.sh
 
-cd ../../
-echo "[x] successfully built distributor page "
+# cd ../../
+# echo "[x] successfully built distributor page "
 
 echo "[ ] starting docker-compose images ... "
 docker-compose up
