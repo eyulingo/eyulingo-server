f2c1f52789352ce2863959cf0679443b5d742afb
yuetsin <akaza_akari@sjtu.edu.cn>
2019-07-11 10:36:23 +0800

    [fix] sequence issues

--- package/run_compose.sh
+++ package/run_compose.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-echo "[ ] removing obsolete images ... "
-docker images | grep -E "(eyulingo-server|eyulingo-mysql)" | awk '{print $3}' | uniq | xargs -I {} docker rmi --force {}
-echo "[*] removed obsolete images"
-
 echo "[ ] stopping docker-compose images ... "
 docker-compose stop
 echo "[*] stopped docker-compose images"
@@ -11,6 +7,10 @@ echo "[*] stopped docker-compose images"
 echo "[ ] removing docker-compose images ... "
 echo y | docker-compose rm
 echo "[*] removed docker-compose images"
+
+echo "[ ] removing obsolete images ... "
+docker images | grep -E "(eyulingo-server|eyulingo-mysql)" | awk '{print $3}' | uniq | xargs -I {} docker rmi --force {}
+echo "[*] removed obsolete images"
 
 echo "[ ] starting docker-compose images ... "
 docker-compose up
