ab4b7ba672c40477ead44ec7aa1725a1d5cad902
zsh99 <1217286706@qq.com>
2019-07-11 17:17:30 +0800

    [add] UnitTest

--- eyulingo/src/test/java/io/github/eyulingo/Service/AdminServiceTest.java
+++ eyulingo/src/test/java/io/github/eyulingo/Service/AdminServiceTest.java
@@ -1,9 +1,13 @@
 package io.github.eyulingo.Service;
 
+import net.sf.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.jar.JarEntry;
 
 import static org.junit.Assert.*;
 
@@ -11,5 +15,28 @@ import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AdminServiceTest {
+    @Autowired
+    private AdminService adminService;
+    @Test
+    public void modifyStoresTest(){
+        JSONObject data = new JSONObject();
+        data.accumulate("store_id",1 );
+        data.accumulate("name","二餐便利店");
+        data.accumulate("address", "地址，某地");
+        data.accumulate("starttime", "28:00" );
+        data.accumulate("endtime","-0:00" );
+        data.accumulate("store_phone_nu","电话号码");
+        assertEquals("{\"status\": \"ok\"}",adminService.modifyStores(data));
+    }
 
+    @Test
+    public  void modifyDistTest(){
+        JSONObject data = new JSONObject();
+        data.accumulate("store_id",1 );
+        data.accumulate("location", "家庭地址");
+        data.accumulate("truename","真实姓名");
+        data.accumulate("dist_phone_nu", "经销商名字");
+        data.accumulate("password","123456");
+        assertEquals("{\"status\": \"ok\"}",adminService.modifyDist(data));
+    }
 }
--- eyulingo/src/test/java/io/github/eyulingo/Service/StoreServiceTest.java
+++ eyulingo/src/test/java/io/github/eyulingo/Service/StoreServiceTest.java
@@ -1,13 +1,70 @@
 package io.github.eyulingo.Service;
 
+import io.github.eyulingo.Service.StoreService;
+import net.sf.json.JSONObject;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import static net.sf.json.test.JSONAssert.assertEquals;
 
-import static org.junit.Assert.*;
 
-@RunWith(SpringRunner.class)
+/**
+ * Created by admin on 2017/9/21-11:33.
+ * Description :
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class StoreServiceTest {
+    @Autowired
+    private StoreService storeService;
 
+    @Test
+    public void getDistTest(){
+        JSONObject item =new JSONObject();
+        item.accumulate("store_id",1);
+        item.accumulate("location","上海市静安区中华新路479号");
+        item.accumulate("truename","乌绮玉");
+        item.accumulate("dist_phone_nu","13640698865");
+        item.accumulate("password","Wuqiyu123456");
+        item.accumulate("dist_image_id","5d1d5e47634459000715143b");
+        assertEquals(item, storeService.getDist("乌绮玉"));
+    }
+
+    @Test
+    public void modifityDistTest(){
+        JSONObject item =new JSONObject();
+        item.accumulate("location","香港特別行政區黃大仙區新蒲崗大有街34號");
+        item.accumulate("truename","方1语");
+        item.accumulate("dist_phone_nu","53542013");
+        item.accumulate("password","Fangyu123456");
+        assertEquals("{\"status\": \"ok\"}",storeService.modifyDist("方语",item));
+
+    }
+
+    @Test
+    public void getMyStoreTest(){
+        assertEquals("13640698865", storeService.getDist("乌绮玉").getString("dist_phone_nu"));
+    }
+
+    @Test
+    public void changeDistImageTest(){
+        JSONObject data = new JSONObject();
+        data.accumulate("image_id","7896145");
+        storeService.ChangeDistImage("迟幼枫",data);
+        assertEquals("7896145",storeService.getDist("迟幼枫").getString("dist_image_id"));
+    }
+
+    @Test
+    public void changeMyStoreTesr(){
+        JSONObject item = new JSONObject();
+        item.accumulate("name","店铺名称");
+        item.accumulate("address", "址");
+        item.accumulate("starttime","营业开始间");
+        item.accumulate("endtime","营业终止时间");
+        item.accumulate("phone_nu", "联系电话");
+        assertEquals("{\"status\": \"ok\"}",storeService.changeMyStore(item,"尚晴"));
+
+    }
 }
