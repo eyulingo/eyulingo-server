43da611ca92553f3877e63e81ba2a823b7da4d19
zsh99 <1217286706@qq.com>
2019-07-18 15:10:12 +0800

    [fix] cart

--- eyulingo/src/test/java/io/github/eyulingo/Service/UserServiceModifityTest.java
+++ eyulingo/src/test/java/io/github/eyulingo/Service/UserServiceModifityTest.java
@@ -1,6 +1,8 @@
 package io.github.eyulingo.Service;
 
+import io.github.eyulingo.Service.UserService;
 import net.sf.json.JSONObject;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,23 +11,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.jar.JarEntry;
-
-import static org.junit.Assert.*;
+import static net.sf.json.test.JSONAssert.assertEquals;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 
+/**
+ * Created by admin on 2017/9/21-11:33.
+ * Description :
+ */
 @Transactional
 @Rollback()
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-public class AdminServiceTest {
+public class UserServiceModifityTest {
+    @Autowired
+    private UserService userService;
+
     private MockMvc mockMvc;
 
     @Autowired
@@ -36,31 +42,26 @@ public class AdminServiceTest {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).apply(springSecurity()).build();
     }
 
-    @Autowired
-    private AdminService adminService;
-
     @Test
-    @WithMockUser(username="Admin1")
-    public void modifyStoresTest(){
-        JSONObject data = new JSONObject();
-        data.accumulate("store_id",1 );
-        data.accumulate("name","二餐便利店");
-        data.accumulate("address", "地址，某地");
-        data.accumulate("starttime", "28:00" );
-        data.accumulate("endtime","-0:00" );
-        data.accumulate("store_phone_nu","电话号码");
-        assertEquals("{\"status\": \"ok\"}",adminService.modifyStores(data));
+    @WithMockUser(username="无竹")
+    public void changeImageTest() {
+        JSONObject item = new JSONObject();
+        item.accumulate("avatar_id","123");
+        userService.changeImage(item);
+        assertEquals("123", userService.getMe().getString("avatar"));
     }
 
     @Test
-    @WithMockUser(username="Admin1")
-    public  void modifyDistTest(){
-        JSONObject data = new JSONObject();
-        data.accumulate("store_id",1 );
-        data.accumulate("location", "家庭地址");
-        data.accumulate("truename","真实姓名");
-        data.accumulate("dist_phone_nu", "经销商名字");
-        data.accumulate("password","123456");
-        assertEquals("{\"status\": \"ok\"}",adminService.modifyDist(data));
+    @WithMockUser(username="无竹")
+    public void changePasswordTest(){
+        JSONObject item = new JSONObject();
+        item.accumulate("origin_password","Wuzhu123456");
+        item.accumulate("new_password","a123456789");
+        item.accumulate("confirm_new_password","a123456789");
+        userService.changePassword(item);
+        assertEquals("a123456789", userService.getPassword());
     }
+
+
+
 }
